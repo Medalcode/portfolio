ca5b1a2a566341fad44db644b9c74681b999152a
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2024-12-30 10:36:18 -0300

    ok

--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '../../images/logo.png'
+import logo from '/src/assets/Images/logo.png'
 
 const Navbar = () => {
   return (
@@ -8,12 +8,23 @@ const Navbar = () => {
       <nav className="nav-wrapper">
         <div className="nav-content">
           <img src={logo} alt="" className="logo" />
+          <ul>
+            <li>
+              <a href="" className="menu-item">Home</a>
+            </li>
+            <li>
+              <a href="" className="menu-item">Skills</a>
+            </li>
+            <li>
+              <a href="" className="menu-item">Work Experience</a>
+            </li>
+            <li>
+              <a href="" className="menu-item">Contact Me</a>
+            </li>
+          </ul>
         </div>
-
-
       </nav>
     </>
-    
   )
 }
 
