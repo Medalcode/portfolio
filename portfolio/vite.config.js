1c2b2ea4eaa774e10c4d952988d6737a3b356542
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2024-12-31 02:11:59 -0300

    20

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// filepath: /C:/Users/forge/Documents/GitHub/portfolio/portfolio/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Asegúrate de que esta ruta coincida con el nombre de tu repositorio
   plugins: [react()],
-})
+});
