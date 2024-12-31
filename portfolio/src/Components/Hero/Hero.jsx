1ab488db28b015273f1182a2b9059f39a4834655
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2024-12-31 03:47:35 -0300

    ok

--- portfolio/src/Components/Hero/Hero.jsx
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './Hero.css'
+import profile from '../../assets/Images/profile.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="hero-container">
+        <div className="hero-content">
+            <h2>Building Digital Experiences That Inspire</h2>
+            <p>Hi, I'm John Doe, a freelance web developer. I can 
+                provide clean, responsive and interactive websites.
+            </p>
+        </div>
+        <div className="hero-img">
+            <div>
+                <div className="tech-icon">
+                    <img src={profile} alt="" className="" />
+                </div>
+                <img src={profile} alt="" className="" />
+            </div>
+            <div>
+                <div className="tech-icon">
+                    <img src={profile} alt="" className="" />
+                </div>
+                <div className="tech-icon">
+                    <img src={profile} alt="" className="" />
+                </div>
+                <div className="tech-icon">
+                    <img src={profile} alt="" className="" />
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
