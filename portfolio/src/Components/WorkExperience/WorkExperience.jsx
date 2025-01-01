734f1333500b8f1612cfa356d7dda34830ece7e5
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2025-01-01 17:48:59 -0300

    42

--- portfolio/src/Components/WorkExperience/WorkExperience.jsx
+++ portfolio/src/Components/WorkExperience/WorkExperience.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './WorkExperience.css';
 import { WORK_EXPERIENCE } from '../../utils/data'; 
 import ExperienceCard from './ExperienceCard/ExperienceCard';
+import Slider from 'react-slick';
+
 
 const WorkExperience = () => {
+  const slideRef = useRef();
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+
   return (
     <section className="experience-container">
       <h5>Work Experience</h5>
 
       <div className="experience-content">
+        <Slider ref={slideRef} {...settings}>
         {WORK_EXPERIENCE.map((item) => (
           <ExperienceCard key={item.title} details={item} />
         ))}
+        </Slider>
       </div>
     </section>
   );
