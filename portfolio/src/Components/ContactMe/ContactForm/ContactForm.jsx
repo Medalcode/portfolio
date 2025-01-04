b63841e5d8e1f678bb041a6dccd5c6c8a36eb7ff
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2025-01-04 03:57:27 -0300

    55

--- portfolio/src/Components/ContactMe/ContactForm/ContactForm.jsx
+++ portfolio/src/Components/ContactMe/ContactForm/ContactForm.jsx
@@ -3,7 +3,17 @@ import './ContactForm.css'
 
 const ContactForm = () => {
   return (
-    <div>ContactForm</div>
+    <div className="contact-form-content">
+        <form action="">
+            <div className="name-container">
+                <input type="text" name="firstname" placeholder='First Name' />
+                <input type="text" name="lastname" placeholder='Last Name' />
+            </div>
+            <input type="email" name="email" placeholder='Email' />
+            <textarea name="message" placeholder='Message' rows={3} />
+            <button type="submit">Send</button>
+        </form>
+    </div>
   )
 }
 
