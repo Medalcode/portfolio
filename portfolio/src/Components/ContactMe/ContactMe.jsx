e289a15ab2ebb03913446867c5a3757b4462c747
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2025-01-04 03:35:24 -0300

    54

--- portfolio/src/Components/ContactMe/ContactMe.jsx
+++ portfolio/src/Components/ContactMe/ContactMe.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './ContactMe.css'
 import ContactInfoCard from './ContactInfoCard/ContactInfoCard'
+import ContactForm from './ContactForm/ContactForm'
 
 const ContactMe = () => {
   return (
@@ -11,20 +12,24 @@ const ContactMe = () => {
         <div className="contact-content">
             <div style={{flex:1}}>
             <ContactInfoCard
-                iconUrl="assets/Images/email.svg"
+                iconUrl="assets/Images/gmail.svg"
                 text="jonatthan.medalla@gmail.com"
             />
             <ContactInfoCard
-                iconUrl="assets/Images/email.svg"
-                text="jonatthan.medalla@gmail.com"
+                iconUrl="assets/Images/github.svg"
+                text="https://github.com/Medalcode"
             />
             <ContactInfoCard
-                iconUrl="assets/Images/email.svg"
-                text="jonatthan.medalla@gmail.com"
+                iconUrl="assets/Images/linkedin.svg"
+                text="www.linkedin.com/in/jonatthan-rodrigo-medalla-aliste-b0672a249
+
+"
             />
         </div>
             
-            <div style={{flex:1}}></div>
+            <div style={{flex:1}}>
+                <ContactForm />
+            </div>
 
         </div>
     </section>
