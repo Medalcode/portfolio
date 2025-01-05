38604f6c22097e523988eca6c187d2d5c962fc06
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2025-01-05 00:47:32 -0300

    58

--- portfolio/src/Components/ContactMe/ContactForm/ContactForm.jsx
+++ portfolio/src/Components/ContactMe/ContactForm/ContactForm.jsx
@@ -4,13 +4,17 @@ import './ContactForm.css'
 const ContactForm = () => {
   return (
     <div className="contact-form-content">
-        <form action="">
+        <form action="https://api.web3forms.com/submit" method="POST">
+        <input type="hidden" name="access_key" value="dc65838a-ee01-4601-8cfe-a3f9fe309c88" />
+
             <div className="name-container">
                 <input type="text" name="firstname" placeholder='First Name' />
                 <input type="text" name="lastname" placeholder='Last Name' />
             </div>
             <input type="email" name="email" placeholder='Email' />
             <textarea name="message" placeholder='Message' rows={3} />
+            <input type="hidden" name="redirect" value="https://web3forms.com/success" />
+
             <button type="submit">Send</button>
         </form>
     </div>
